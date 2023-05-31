2ba06b596bd04170fc28110f3eac9b62e59012ec
vmred <16134967+vmred@users.noreply.github.com>
2023-05-31 23:43:53 +0300

    lesson 3: add simple integration test

--- lessons/lesson_3/app/flask_start.py
+++ lessons/lesson_3/app/flask_start.py
@@ -21,7 +21,6 @@ def get_rates():
 @app.route("/rates_pb", methods=['GET'])
 def get_pb_rates():
     convert_currency = request.args.get('convert_currency', default='USD')
-    bank = request.args.get('bank', default='NBU') # TODO додати функцію валідації вводу банку
+    bank = request.args.get('bank', default='NBU')
     rate_date = request.args.get('rate_date', default='01.11.2022')
-    result = get_pb_exchange_rate(convert_currency, bank, rate_date)
-    return result
+    return get_pb_exchange_rate(convert_currency, bank, rate_date)
