3422e25fedd01f200b4687083d171a6439b9164b
vmred <16134967+vmred@users.noreply.github.com>
2023-06-05 14:37:49 +0300

    [lesson 4 homework] - remove xfail and update report file (#9)

--- lessons/lesson_3/tests/test_db_practise.py
+++ lessons/lesson_3/tests/test_db_practise.py
@@ -6,11 +6,9 @@ from lessons.lesson_3.app.db_practice import get_invoice_items_profit, get_repea
 class TestDbPractise:
 
     @pytest.mark.homework4
-    @pytest.mark.xfail(raises=AssertionError, reason='db data might change')
     def test_get_invoice_items_profit(self):
         assert get_invoice_items_profit() == 2328.599999999957
 
     @pytest.mark.homework4
-    @pytest.mark.xfail(raises=AssertionError, reason='db data might change')
     def test_get_repeatable_customers(self):
         assert get_repeatable_customers() == [('Frank', 2), ('Mark', 2)]
