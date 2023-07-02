ef879e83892b9c44d7bfeb17b33cc887a3f14d3b
vmred <16134967+vmred@users.noreply.github.com>
2023-07-02 18:31:31 +0300

    make pan to be unique

--- lessons/lesson_11/homework_9/cards/models/card.py
+++ lessons/lesson_11/homework_9/cards/models/card.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Card(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    pan = models.CharField(max_length=16)
+    pan = models.CharField(max_length=16, unique=True)
     expiry_date = models.DateField()
     cvv = models.CharField(max_length=3)
     issue_date = models.DateField()
