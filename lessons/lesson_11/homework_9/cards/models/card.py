6b6c09239143392d709cdf1cae92c2a9a3841e3a
vmred <16134967+vmred@users.noreply.github.com>
2023-07-02 12:58:07 +0300

    [lesson 11]: homework 9 initial commit

---
+++ lessons/lesson_11/homework_9/cards/models/card.py
@@ -0,0 +1,13 @@
+import uuid
+
+from django.db import models
+
+
+class Card(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    pan = models.CharField(max_length=16)
+    expiry_date = models.DateField()
+    cvv = models.CharField(max_length=3)
+    issue_date = models.DateField()
+    owner_id = models.UUIDField(default=uuid.uuid4)
+    status = models.CharField()
