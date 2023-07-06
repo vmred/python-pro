ec9c3e18eb2dc10a5e48b02e61b7400459ab0c93
vmred <16134967+vmred@users.noreply.github.com>
2023-07-07 02:48:48 +0300

    [lesson 12]: homework

--- lessons/lesson_11/homework_9/cards/tests/test_integration.py
+++ lessons/lesson_11/homework_9/cards/tests/test_integration.py
@@ -64,9 +64,30 @@ class TestIntegration:
         card = Card(pan='38520000023237')
         card.save()
         url = reverse('cards')
-        response = client.get(url)
+        response = client.get(url, headers={'accept': 'application/json'})
         response_json = response.json()
         assert response.status_code == 200
         assert response_json[0]['pan'] == '38520000023237'
         assert response_json[0]['issue_date'] == self.time_now.date().strftime('%Y-%m-%d')
         assert response_json[0]['expiry_date'] == (self.time_now + timedelta(days=365 * 2)).date().strftime('%Y-%m-%d')
+
+    def test_cards_template(self, client):
+        response = client.get(reverse('cards_create_form'))
+        assert response.status_code == 200
+
+    def test_cards_view_form(self, client):
+        response = client.get('/cards/', headers=None, cookies=None, secure=False)
+        assert response.status_code == 200
+        assert (
+            response.content
+            == b'<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>Cards</title>\n</head>\n<style>\n\ntable, th, td {border: 1px solid black;  border-collapse: collapse;}\n\n</style>\n<body>\n \n\n\n<p>No data</p>\n\n\n</body>\n</html>'
+        )
+
+    def test_cards_create_form(self, client):
+        response = client.get('/cards/create')
+        assert response.status_code == 200
+        csrf_token = response.context.get('csrf_token')
+        assert (
+            response.content
+            == f'<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>Cards</title>\n</head>\n<style>\n \n</style>\n<body>\n\n<form action="/cards/create" method="post">\n    <input type="hidden" name="csrfmiddlewaretoken" value="{csrf_token}">\n    <fieldset>\n        <legend><h1>Create new card</h1></legend>\n        <div class="pan">\n            <label>Pan</label>\n            <input type="text" name="pan">\n        </div>\n        <div class="issue_date">\n            <label>Issue date</label>\n            <input type="date" name="issue_date">\n        </div>\n\n        <div class="expiry_date">\n            <label>Expiry date</label>\n            <input type="date" name="expiry_date">\n        </div>\n\n        <div class="cvv">\n            <label>cvv</label>\n            <input type="text" name="cvv">\n        </div>\n\n        <div class="owner_id">\n            <label>Owner id</label>\n            <input type="text" name="owner_id">\n        </div>\n\n        <div class="status">\n            <label>Status</label>\n            <select name="status" id="status">\n\n                <option value="new">new</option>\n                <option value="active">active</option>\n        </select>\n        </div>\n    </fieldset>\n    <input type="submit" value="Create">\n</form>\n\n \n</body>\n</html>'.encode()
+        )
--- lessons/lesson_11/homework_9/cards/views/card.py
+++ lessons/lesson_11/homework_9/cards/views/card.py
@@ -1,6 +1,8 @@
 import json
 
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
 
 from ..models.card import Card
@@ -10,21 +12,22 @@ class CardView(View):
     @staticmethod
     def get(request: HttpRequest):  # pylint: disable=unused-argument
         cards = Card.objects.all()  # pylint: disable=no-member
-        return JsonResponse(
-            [
-                {
-                    'id': str(card.id),
-                    'pan': card.pan,
-                    'expiry_date': card.expiry_date,
-                    'cvv': card.cvv,
-                    'issue_date': card.issue_date,
-                    'owner_id': card.owner_id,
-                    'status': card.status,
-                }
-                for card in cards
-            ],
-            safe=False,
-        )
+        context = [
+            {
+                'id': str(card.id),
+                'pan': card.pan,
+                'expiry_date': card.expiry_date,
+                'cvv': card.cvv,
+                'issue_date': card.issue_date,
+                'owner_id': card.owner_id,
+                'status': card.status,
+            }
+            for card in cards
+        ]
+        if request.headers.get('accept') == 'application/json':
+            return JsonResponse(context, safe=False)
+
+        return render(request, 'cards/cards_view_form.html', {'cards': context}, 'text/html')
 
     @staticmethod
     def post(request: HttpRequest):
@@ -39,8 +42,28 @@ class CardView(View):
             expiry_date=request_body.get('expiry_date'),
             issue_date=request_body.get('issue_date'),
             cvv=request_body['cvv'],
-            owner_id=request_body['owner_id'],
+            owner_id=int(request_body['owner_id']),
             status=request_body['status'],
         )
         card.save()
         return JsonResponse({'id': str(card.id)})
+
+
+def cards_create_form(request):
+    if request.method == 'GET':
+        # pylint: disable=no-member
+        return render(request, 'cards/cards_create_form.html', {'cards': Card.objects.all()})
+
+    if request.method == 'POST':
+        # pylint: disable=no-member
+        Card.objects.create(
+            pan=request.POST['pan'],
+            expiry_date=request.POST['expiry_date'],
+            issue_date=request.POST['issue_date'],
+            cvv=request.POST['cvv'],
+            owner_id=int(request.POST['owner_id']),
+            status=request.POST['status'],
+        )
+        return HttpResponseRedirect(reverse('cards'))
+
+    return render(request, 'cards/invalid_request.html')
