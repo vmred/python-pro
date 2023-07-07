06ca8c29bbe811a64b10ab5a1debe76cfea090d1
vmred <16134967+vmred@users.noreply.github.com>
2023-07-07 09:15:56 +0300

    [lesson 12]: homework (#19)

---
+++ lessons/lesson_11/homework_9/cards/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models.card import Card
+
+admin.site.register(Card)
--- lessons/lesson_11/homework_9/cards/urls.py
+++ lessons/lesson_11/homework_9/cards/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views.card import CardView
+from .views.card import CardView, cards_create_form
 
 urlpatterns = [
     path('cards/', CardView.as_view(http_method_names=['get', 'post']), name='cards'),
+    path("cards/create", cards_create_form, name="cards_create_form"),
 ]
