f02d86982bdaff3af939c719d5ac238b7786ebfa
vmred <16134967+vmred@users.noreply.github.com>
2023-07-27 22:13:15 +0300

    * add celery beat task to block expired cards

--- lessons/lesson_14/cards/urls.py
+++ lessons/lesson_14/cards/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views.card import CardView, activate_card, activate_card_task, deactivate_card
+from .views.card import CardView, activate_card, activate_card_task, deactivate_card, get_expired_cards
 
 urlpatterns = [
     path('cards/', CardView.as_view(), name='cards'),
     path("cards/<int:pk>", CardView.as_view()),
     path('cards/activate/<int:pk>', activate_card),
     path('cards/tasks/activate/<int:pk>', activate_card_task),
+    path('cards/expired', get_expired_cards),
     path('cards/deactivate/<int:pk>', deactivate_card),
 ]
--- lessons/lesson_14/cards/views/card.py
+++ lessons/lesson_14/cards/views/card.py
@@ -1,6 +1,8 @@
+from datetime import date
 import time
 
 from celery import shared_task
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status
@@ -8,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..models.card import Card
+from ..models.card import Card, Status
 from ..permissions import IsOwner
 from ..serializers.card import CardSerializer, UpdateCardSerializer
 
@@ -23,6 +25,20 @@ def object_exists(model, request, pk):
 @shared_task
 def task_activate_card(pk: str):
     Card.activate_card(pk)
+
+
+def get_expired_cards(request):
+    cards = Card.objects.filter(~Q(status=Status.BLOCKED), expiry_date__lt=date.today())
+    return JsonResponse(CardSerializer(cards, many=True).data, safe=False)
+
+
+@shared_task
+def block_expired_cards_task():
+    cards = Card.objects.filter(~Q(status=Status.BLOCKED), expiry_date__lt=date.today())
+    for card in cards:
+        print(f'blocking card with id: {card.id}')
+        Card.deactivate_card(card.id)
+    return True
 
 
 def activate_card_task(request, pk):
@@ -59,11 +75,11 @@ class CardView(APIView):
     def get(self, request, pk=None):
         if not pk:
             cards = Card.objects.filter(owner=request.user)
-            return Response(CardSerializer(cards, many=True).data)
+            return JsonResponse(CardSerializer(cards, many=True).data, safe=False)
 
         card = get_object_or_404(Card, pk=pk)
         self.check_object_permissions(self.request, card)
-        return Response(CardSerializer(card).data)
+        return JsonResponse(CardSerializer(card).data)
 
     def post(self, request):
         Card.validate_card_requests_fields(request.data)
@@ -80,16 +96,16 @@ class CardView(APIView):
             printed_name=request.data.get('printed_name', ''),
         )
         card.save()
-        return Response({'id': str(card.id)})
+        return JsonResponse({'id': str(card.id)})
 
     def put(self, request, pk):
         card = object_exists(model=Card, request=request, pk=pk)
         if not card:
-            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = UpdateCardSerializer(card, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({'success': True})
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
