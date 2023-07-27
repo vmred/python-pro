872765050b1c87f5fadbbd37062ebbda05e59e62
vmred <16134967+vmred@users.noreply.github.com>
2023-07-27 22:52:45 +0300

    add test on block all expired cards task

--- lessons/lesson_14/cards/tests/test_tasks.py
+++ lessons/lesson_14/cards/tests/test_tasks.py
@@ -1,14 +1,19 @@
+from datetime import date, datetime, timedelta
+
 import pytest
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.test.utils import override_settings
 
 from ..models.card import Card, Status
 from ..tests import get_card_cvv, get_card_number, get_name
-from ..views.card import task_activate_card
+from ..views.card import block_expired_cards_task, task_activate_card
 
 
 @pytest.mark.django_db
 class TestTasks:
+    time_now = datetime.now()
+
     @override_settings(CELERY_TASK_ALWAYS_EAGER=True, CELERY_TASK_EAGER_PROPOGATES=True)
     def test_task_activate_card(self):
         user = User.objects.create_user(username='12345', password='12345')
@@ -19,3 +24,23 @@ class TestTasks:
         task_activate_card.delay(model.id)
         model.refresh_from_db()
         assert model.status == Status.ACTIVE
+
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True, CELERY_TASK_EAGER_PROPOGATES=True)
+    def test_task_block_all_expired_cards(self):
+        user = User.objects.create_user(username='12345', password='12345')
+        for _ in range(2):
+            Card.objects.create(
+                pan=get_card_number(),
+                cvv=get_card_cvv(),
+                status='new',
+                printed_name=get_name(),
+                owner=user,
+                issue_date=self.time_now - timedelta(days=100),
+                expiry_date=self.time_now - timedelta(days=2),
+            )
+
+        cards = Card.objects.filter(~Q(status=Status.BLOCKED), expiry_date__lt=date.today())
+        assert len(cards) == 2
+        block_expired_cards_task.delay()
+        cards = Card.objects.filter(~Q(status=Status.BLOCKED), expiry_date__lt=date.today())
+        assert len(cards) == 0
