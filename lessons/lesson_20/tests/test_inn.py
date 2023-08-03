d53fb6f3129c8b7fa3ab2d63e2c4a5f406702cf2
vmred <16134967+vmred@users.noreply.github.com>
2023-08-03 21:43:47 +0300

    [lesson 20]: homework

---
+++ lessons/lesson_20/tests/test_inn.py
@@ -0,0 +1,32 @@
+import pytest
+
+from ..regex import is_inn_valid
+
+
+class TestInn:
+
+    @pytest.mark.parametrize('inn, is_valid', [
+        ('!@#$%^', False),
+        (r'.*', False),
+        ('1234567897', True),
+        ('0000000000', True),
+        ('000000009', False),
+        ('00000000000', False),
+        ('', False),
+        ('zxc', False),
+        ('qwertyuiop', False),
+        ('123456789P', False),
+        ('o123456789', False),
+    ])
+    def test_inn_match(self, inn, is_valid):
+        assert is_inn_valid(inn) == is_valid
+
+    @pytest.mark.parametrize('inn, exception', [
+        (None, TypeError),
+        ([], TypeError),
+        ({}, TypeError),
+        (['1234567897'], TypeError)
+    ])
+    def test_inn_match_exception(self, inn, exception):
+        with pytest.raises(exception):
+            is_inn_valid(inn)
