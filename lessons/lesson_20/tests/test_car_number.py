3a200b70060e60dcafa6b7d9e967821e7bd71e22
vmred <16134967+vmred@users.noreply.github.com>
2023-08-03 21:44:34 +0300

    apply linters

--- lessons/lesson_20/tests/test_car_number.py
+++ lessons/lesson_20/tests/test_car_number.py
@@ -1,57 +1,57 @@
 import pytest
 
-from ..regex import is_car_number_valid_kharkiv, is_car_number_valid_dnepr
+from ..regex import is_car_number_valid_dnepr, is_car_number_valid_kharkiv
 
 
 class TestCarNumber:
-    @pytest.mark.parametrize('car_number, is_valid', [
-        ('!@#$%^', False),
-        (r'.*', False),
-        ('', False),
-        ('XX0000AE', True),
-        ('EX0001TP', True),
-        ('AX0002HC', True),
-        ('KX0003BE', True),
-        ('AA1234AA', False),
-        ('aa0001AA', False),
-        ('KX12345AA', False),
-        ('KX123OI', False)
-    ])
+    @pytest.mark.parametrize(
+        'car_number, is_valid',
+        [
+            ('!@#$%^', False),
+            (r'.*', False),
+            ('', False),
+            ('XX0000AE', True),
+            ('EX0001TP', True),
+            ('AX0002HC', True),
+            ('KX0003BE', True),
+            ('AA1234AA', False),
+            ('aa0001AA', False),
+            ('KX12345AA', False),
+            ('KX123OI', False),
+        ],
+    )
     def test_car_number_match_kharkiv(self, car_number, is_valid):
         assert is_car_number_valid_kharkiv(car_number) == is_valid
 
-    @pytest.mark.parametrize('car_number, is_valid', [
-        ('!@#$%^', False),
-        (r'.*', False),
-        ('', False),
-        ('PP0000AE', True),
-        ('MI0001TP', True),
-        ('AE0002HC', True),
-        ('KE0003BE', True),
-        ('AA1234AA', False),
-        ('aa0001AA', False),
-        ('KX12345AA', False),
-        ('KX123OI', False)
-    ])
+    @pytest.mark.parametrize(
+        'car_number, is_valid',
+        [
+            ('!@#$%^', False),
+            (r'.*', False),
+            ('', False),
+            ('PP0000AE', True),
+            ('MI0001TP', True),
+            ('AE0002HC', True),
+            ('KE0003BE', True),
+            ('AA1234AA', False),
+            ('aa0001AA', False),
+            ('KX12345AA', False),
+            ('KX123OI', False),
+        ],
+    )
     def test_car_number_match_dnepr(self, car_number, is_valid):
         assert is_car_number_valid_dnepr(car_number) == is_valid
 
-    @pytest.mark.parametrize('car_number, exception', [
-        (None, TypeError),
-        ([], TypeError),
-        ({}, TypeError),
-        (['MT2334PI'], TypeError)
-    ])
+    @pytest.mark.parametrize(
+        'car_number, exception', [(None, TypeError), ([], TypeError), ({}, TypeError), (['MT2334PI'], TypeError)]
+    )
     def test_car_number_kharkiv_match_exception(self, car_number, exception):
         with pytest.raises(exception):
             is_car_number_valid_kharkiv(car_number)
 
-    @pytest.mark.parametrize('car_number, exception', [
-        (None, TypeError),
-        ([], TypeError),
-        ({}, TypeError),
-        (['MT2334PI'], TypeError)
-    ])
+    @pytest.mark.parametrize(
+        'car_number, exception', [(None, TypeError), ([], TypeError), ({}, TypeError), (['MT2334PI'], TypeError)]
+    )
     def test_car_number_dnepr_match_exception(self, car_number, exception):
         with pytest.raises(exception):
             is_car_number_valid_dnepr(car_number)
--- lessons/lesson_20/tests/test_passport_number.py
+++ lessons/lesson_20/tests/test_passport_number.py
@@ -4,30 +4,30 @@ from ..regex import is_passport_valid
 
 
 class TestPassportNumber:
-    @pytest.mark.parametrize('passport_number, is_valid', [
-        ('KM123456', True),
-        ('KA1234567', False),
-        ('KA12345', False),
-        ('00123456', False),
-        ('', False),
-        ('ЯЮ1234009', False),
-        ('MT234300\n', True),
-        ('MT233400 MT233400', False),
-        ('123456', False),
-        ('BKMBTC', False),
-        ('bk122143', False),
-        ('!@#$%^', False),
-        (r'.*', False)
-    ])
+    @pytest.mark.parametrize(
+        'passport_number, is_valid',
+        [
+            ('KM123456', True),
+            ('KA1234567', False),
+            ('KA12345', False),
+            ('00123456', False),
+            ('', False),
+            ('ЯЮ1234009', False),
+            ('MT234300\n', True),
+            ('MT233400 MT233400', False),
+            ('123456', False),
+            ('BKMBTC', False),
+            ('bk122143', False),
+            ('!@#$%^', False),
+            (r'.*', False),
+        ],
+    )
     def test_passport_match(self, passport_number, is_valid):
         assert is_passport_valid(passport_number) == is_valid
 
-    @pytest.mark.parametrize('passport_number, exception', [
-        (None, TypeError),
-        ([], TypeError),
-        ({}, TypeError),
-        (['MT233400'], TypeError)
-    ])
+    @pytest.mark.parametrize(
+        'passport_number, exception', [(None, TypeError), ([], TypeError), ({}, TypeError), (['MT233400'], TypeError)]
+    )
     def test_passport_match_exception(self, passport_number, exception):
         with pytest.raises(exception):
             is_passport_valid(passport_number)
