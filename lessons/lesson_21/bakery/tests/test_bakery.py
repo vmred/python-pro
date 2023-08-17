8effd91cd5f80c72e937c5358ea2fb831511c7bc
vmred <16134967+vmred@users.noreply.github.com>
2023-08-17 22:17:31 +0300

    apply linters

--- lessons/lesson_21/bakery/tests/test_bakery.py
+++ lessons/lesson_21/bakery/tests/test_bakery.py
@@ -3,7 +3,6 @@ import pytest
 
 @pytest.mark.lesson_21_homework
 class TestBakery:
-
     def test_bakery(self, application):
         request, response = application.get('/bakery')
         assert response.status_code == 200
--- lessons/lesson_21/ingredients/tests/test_ingredients.py
+++ lessons/lesson_21/ingredients/tests/test_ingredients.py
@@ -3,7 +3,6 @@ import pytest
 
 @pytest.mark.lesson_21_homework
 class TestIngredients:
-
     def test_ingredients(self, application):
         request, response = application.get('/ingredients')
         assert response.status_code == 200
